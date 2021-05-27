93262349627b8366eab22b9cdbec3d12fcb081f7
harris <khanharris7@yahoo.com>
2021-05-26 23:10:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,52 +1,89 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name){
-    cats.push(name);
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
 }
 
-destructivelyAppendCat("Milo", "Otis", "Garfield");
-function destructivelyPrependCat(name){
-    cats.unshift(name);
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
 }
+
 function destructivelyRemoveLastCat(){
     cats.pop();
 }
 
-destructivelyRemoveLastCat();
-function destructivelyRemovefirstCat(){
-    cats.shift('Milo');
-}
-
-
-
-function destructivelyRemoveLastCat(){
-    cats.shift("Milo");
-}
-
 function destructivelyRemoveFirstCat(){
-    cats.shift('Milo');
-
+    cats.shift();
 }
 
-function appendCat(name){
-    return ["Milo", "Otis", "Garfield", "Broom"];
+function appendCat(){
+    let newCatOrder = [...cats, "Broom"];
+    return newCatOrder;
+    
 }
 
-appendCat("Broom");
-function prependCat(name){
-    return ["Arnold", "Milo", "Otis", "Garfield"];
+function prependCat(){
+    let newCatOrder = ["Arnold", ...cats];
+    return newCatOrder;
 }
 
-prependCat("Arnold");
 function removeLastCat(){
-    return cats.slice(0, 2);
+    return cats.slice(0, -1);
+    
+    
 }
 
-removeLastCat();
 function removeFirstCat(){
     return cats.slice(1);
 }
+//function destructivelyAppendCat(name){
+//    cats.push(name);
+//}
 
+//destructivelyAppendCat("Milo", "Otis", "Garfield");
+//function destructivelyPrependCat(name){
+//    cats.unshift(name);
+//}
+//function destructivelyRemoveLastCat(){
+//    cats.pop();
+//}
+
+//destructivelyRemoveLastCat();
+//function destructivelyRemovefirstCat(){
+//   cats.shift('Milo');
+//}
+
+
+
+//function destructivelyRemoveLastCat(){
+//    cats.shift("Milo");
+//}
+
+//function destructivelyRemoveFirstCat(){
+  //  cats.shift('Milo');
+
+//}
+
+//function appendCat(name){
+//    return ["Milo", "Otis", "Garfield", "Broom"];
+//}
+
+//appendCat("Broom");
+//function prependCat(name){
+//    return ["Arnold", "Milo", "Otis", "Garfield"];
+//}
+
+//prependCat("Arnold");
+//function removeLastCat(){
+//    return cats.slice(0, 2);
+//}
+
+//removeLastCat();
+//function removeFirstCat(){
+//    return cats.slice(1);
+//}
 
 
